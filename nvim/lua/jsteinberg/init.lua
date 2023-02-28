66bdb6e2f59ded9b9dce5c8951353c251062a5b2
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-02-27 22:22:05 -0500

    QOL autocommands

--- nvim/lua/jsteinberg/init.lua
+++ nvim/lua/jsteinberg/init.lua
@@ -5,10 +5,14 @@ require("jsteinberg.packer")
 require("jsteinberg.remap")
 require("jsteinberg.set")
 
-local yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = yank_group,
+
+-- Highlight on Yank
+augroup("HighlightYank", { clear = true })
+autocmd('TextYankPost', {
+    group = 'HighlightYank',
     pattern = '*',
     callback = function()
         vim.highlight.on_yank({
@@ -16,4 +20,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
             timeout = 40,
         })
     end,
+})
+
+-- Remember code folds on exit
+augroup('RememberFolds', {})
+autocmd('BufWinEnter', {
+    pattern = '*',
+    command = 'silent! loadview',
+})
+autocmd('BufWinLeave', {
+    pattern = '*',
+    command = 'silent! mkview',
+})
+
+
+
+-- Terminal Settings
+-------------------
+-- Open term in right tab
+autocmd('CmdlineEnter', {
+    command = 'command! Term :botright vsplit term://$SHELL',
+})
+
+-- Enter insert mode on open term
+autocmd('TermOpen', {
+    command = 'setlocal listchars= nonumber norelativenumber nocursorline'
+})
+autocmd('TermOpen', {
+    pattern = '',
+    command = 'startinsert',
 })
