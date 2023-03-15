595d8bbc50cc5cff1b031d4740b5ba82406ea41a
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-03-15 09:36:36 -0400

    Started copying the LazyVim/starter template repo

--- nvim-lazy/lua/plugins/telescope.lua
+++ nvim-lazy/lua/plugins/telescope.lua
@@ -1,17 +1,17 @@
 -- =============================
--- =    Telescope.nvim 
+-- =    Telescope.nvim
 -- =============================
 return {
-'nvim-telescope/telescope.nvim',
-branch = '0.1.x',
-dependencies = {
-    { 'nvim-lua/plenary.nvim' },
-    { -- Faster(?) fuzzy find algorithm
-	'nvim-telescope/telescope-fzf-native.nvim',
-	build = 'make',
-	cond = vim.fn.executable 'make' == 1
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+        { 'nvim-lua/plenary.nvim' },
+        { -- Faster(?) fuzzy find algorithm
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+            cond = vim.fn.executable 'make' == 1
+        },
+        { 'nvim-tree/nvim-web-devicons' },
     },
-    { 'nvim-tree/nvim-web-devicons' },
-},
-event = "VimEnter",
+    event = "VimEnter",
 }
