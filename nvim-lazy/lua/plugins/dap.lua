4c34cd3667bc46dcc9b1f62424d6f2551967592b
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-03-18 18:00:57 -0400

    Copy over DAP and LSP configs

--- nvim-lazy/lua/plugins/dap.lua
+++ nvim-lazy/lua/plugins/dap.lua
@@ -2,7 +2,8 @@
 -- =    Debug Setup (DAP)
 -- =============================
 return {
-    { -- VSCode like debug UI
+    {
+        -- VSCode like debug UI
         "rcarriga/nvim-dap-ui",
         dependencies = {
             { "mfussenegger/nvim-dap" },
@@ -22,3 +23,33 @@ return {
         }
     },
 }
+
+-- Debug settings from old setup. Convert to lazy.nvim.
+-- local DAP = require('dap')
+-- local DAPUI = require('dapui')
+--
+-- -- Configure dap-ui to auto open/close with DAP
+-- DAP.listeners.after.event_initialized["dapui_config"] = function()
+--     DAPUI.open()
+-- end
+-- DAP.listeners.before.event_terminated["dapui_config"] = function()
+--     DAPUI.close()
+-- end
+-- DAP.listeners.before.event_exited["dapui_config"] = function()
+--     DAPUI.close()
+-- end
+--
+-- DAPUI.setup()
+--
+--
+-- require('dap-python').setup(
+--     '/Library/Frameworks/Python.framework/Versions/3.10/bin/python3.10'
+-- )
+--
+--
+-- -- require("neotest").setup({
+-- --     adapters = {
+-- --         -- neotest-python does not have a setup function
+-- --         require("neotest-python")({})
+-- --     }
+-- -- })
