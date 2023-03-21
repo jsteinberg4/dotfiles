5c00cd091a6b3188dd43ce4cc10f9de57f0e5691
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-03-21 01:41:27 -0400

    Telescope keymaps

--- nvim-lazy/lua/jsteinberg/config/options.lua
+++ nvim-lazy/lua/jsteinberg/config/options.lua
@@ -6,7 +6,7 @@ vim.o.relativenumber = true
 vim.o.ruler = true -- Shows cursor position in status line
 
 -- Disable mouse
-vim.o.mouse = 'nv'
+vim.o.mouse = "nv"
 
 vim.o.errorbells = false
 
@@ -45,8 +45,7 @@ vim.o.updatetime = 50 -- Update files often
 vim.o.colorcolumn = "80"
 
 -- Set completeopt for better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
+vim.o.completeopt = "menuone,noselect"
 
 -- NetRW configs
 vim.g.netrw_browse_split = 0
--- nvim-lazy/lua/jsteinberg/plugins/misc.lua
+++ nvim-lazy/lua/jsteinberg/plugins/misc.lua
@@ -3,82 +3,87 @@
 -- =============================
 
 return {
-    {
-        -- Fancier statusline
-        'nvim-lualine/lualine.nvim',
-        event = "VeryLazy",
-        opt = {
-            icons_enabled = false,
-            -- theme = vim.g.my_colors,
-            component_separators = '|',
-            section_separators = '',
-        }
-    },
-    {
-        -- add indent guides on bank lines
-        'lukas-reineke/indent-blankline.nvim',
-        opts = {
-            char = "│",
-            show_trailing_blankline_indent = false,
-            show_current_context = false,
-            filetype_exclude = {
-                "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy"
-            },
-        },
-    },
-    {
-        -- persistent & powerful undo abilities. Load when called.
-        "mbbill/undotree",
-        cmd = { "UndotreeShow", "UndotreeToggle" },
-        keys = {
-            { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle undotree" },
-        },
-    },
+	{
+		-- Fancier statusline
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			icons_enabled = false,
+			-- theme = vim.g.my_colors,
+			component_separators = "|",
+			section_separators = "",
+		},
+	},
+	{
+		-- add indent guides on bank lines
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			char = "│",
+			show_trailing_blankline_indent = false,
+			show_current_context = false,
+			filetype_exclude = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"lazy",
+			},
+		},
+	},
+	{
+		-- persistent & powerful undo abilities. Load when called.
+		"mbbill/undotree",
+		cmd = { "UndotreeShow", "UndotreeToggle" },
+		keys = {
+			{ "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle undotree" },
+		},
+	},
 
-    {
-        -- better window movement
-        's1n7ax/nvim-window-picker',
-        version = 'v1.*',
-        keys = {
-            {
-                "<leader>pw",
-                function()
-                    local picked_id = require("window-picker").pick_window() or vim.api.nvim_get_current_win()
-                    vim.api.nvim_set_current_win(picked_id)
-                end,
-                desc = "[P]ick [W]indow",
-            },
-        },
-        opts = {
-            include_current = false,
-            autoselect_one = true,
-            filter_rules = {
-                bo = {
-                    -- ignore neo-tree or notify windows
-                    filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
-                    -- Ignore terminal and quickfix buffers
-                    buftype = { 'terminal', 'quickfix' },
-                },
-            },
-            other_win_hl_color = "#e35e4f",
-        },
-    },
+	{
+		-- better window movement
+		"s1n7ax/nvim-window-picker",
+		version = "v1.*",
+		keys = {
+			{
+				"<leader>pw",
+				function()
+					local picked_id = require("window-picker").pick_window() or vim.api.nvim_get_current_win()
+					vim.api.nvim_set_current_win(picked_id)
+				end,
+				desc = "[P]ick [W]indow",
+			},
+		},
+		opts = {
+			include_current = false,
+			autoselect_one = true,
+			filter_rules = {
+				bo = {
+					-- ignore neo-tree or notify windows
+					filetype = { "neo-tree", "neo-tree-popup", "notify" },
+					-- Ignore terminal and quickfix buffers
+					buftype = { "terminal", "quickfix" },
+				},
+			},
+			other_win_hl_color = "#e35e4f",
+		},
+	},
 
-    -- Comments
-    { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
-    {
-        "echasnovski/mini.comment",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            hooks = {
-                pre = function()
-                    require("ts_context_commentstring.internal").update_commentstring({})
-                end,
-            },
-        },
-        config = function(_, opts)
-            require("mini.comment").setup(opts)
-        end,
-    },
+	-- Comments
+	{ "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+	{
+		"echasnovski/mini.comment",
+		version = "*",
+		event = "VeryLazy",
+		opts = {
+			hooks = {
+				pre = function()
+					require("ts_context_commentstring.internal").update_commentstring({})
+				end,
+			},
+		},
+		config = function(_, opts)
+			require("mini.comment").setup(opts)
+		end,
+	},
 }
