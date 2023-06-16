08c211ae5dcb8b742a560864636f6b5ff43fed01
jsteinberg <jesse.steinberg@recordedfuture.com>
2023-06-16 11:36:48 -0400

    neovim plugin changes

---
+++ nvim/LazyVim/lua/plugins/disable.lua
@@ -0,0 +1,7 @@
+-- table of pre-configured plugins from LazyVim/starter to disable
+
+return {
+  -- TODO: Disabling flit, sneak until I have a better keymap to get insert/replace mode back (highlight, s)
+  { "ggandor/leap.nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+}
---
+++ nvim/LazyVim/lua/plugins/lsp.lua
@@ -0,0 +1,2 @@
+-- LSP configurations
+return {}
