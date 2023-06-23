fe11da624c84d4baebe652b451d8a693da882546
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-06-23 15:30:49 -0400

    Fancier disabling logic

--- nvim/LazyVim/lua/plugins/disable.lua
+++ nvim/LazyVim/lua/plugins/disable.lua
@@ -1,7 +1,16 @@
 -- table of pre-configured plugins from LazyVim/starter to disable
 
-return {
+local plugins = {}
+local to_disable = {
   -- TODO: Disabling flit, sneak until I have a better keymap to get insert/replace mode back (highlight, s)
-  { "ggandor/leap.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
+  "ggandor/leap.nvim",
+  "ggandor/flit.nvim",
+  "goolord/alpha-nvim",
 }
+
+-- Disable every plugin in the `to_disable` list
+for _, name in pairs(to_disable) do
+  vim.list_extend(plugins, { { name, enabled = false } })
+end
+
+return plugins
