fca8f2da9130d4c22fea6968fdbe4cbdd1d750df
jsteinberg <jesse.steinberg@recordedfuture.com>
2023-06-23 09:59:28 -0400

    Add python support

--- nvim/LazyVim/lua/config/keymaps.lua
+++ nvim/LazyVim/lua/config/keymaps.lua
@@ -7,8 +7,7 @@ local Util = require("lazyvim.util")
 local function map(mode, lhs, func, opts)
   local keys = require("lazy.core.handler").handlers.keys
 
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
+  ---@cast keys LazyKeysHandler do not create the keymap if a lazy keys handler exists
   if not keys.active[keys.parse({ lhs, mode = mode }).id] then
     opts = opts or {}
     opts.silent = opts.silent ~= false
@@ -23,10 +22,10 @@ map("n", "J", "mzJ`z", { desc = "Join lines w/o moving cursor" })
 map("n", "<leader>lz", "<CMD>Lazy<CR>", { desc = "Open [l]a[z]y UI", silent = true })
 map("n", "gg", "ggzz", { desc = "GOTO top, center cursor" })
 map("n", "G", "Gzz", { desc = "GOTO bottom, center cursor" })
-map("n", "<C-d>", "<C-d>zz", { desc = "Page down, center cursor" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Page up, center cursor" })
-map("n", "n", "nzzzv", { desc = "Keep cursor centered while searching" })
-map("n", "N", "Nzzzv", { desc = "Center cursor while searching" })
+map("n", "<C-d>", "<C-d>zt", { desc = "Page down, top cursor" })
+map("n", "<C-u>", "<C-u>zt", { desc = "Page up, top cursor" })
+map("n", "n", "nztzv", { desc = "Keep cursor centered while searching" })
+map("n", "N", "Nztzv", { desc = "Center cursor while searching" })
 
 -- Moving lines
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down", silent = true })
--- nvim/LazyVim/lua/plugins/lsp.lua
+++ nvim/LazyVim/lua/plugins/lsp.lua
@@ -17,43 +17,6 @@ return {
     },
   },
   {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- Any servers here will be automatically installed with mason and loaded with lspconfig
-        lua_ls = { -- Lua language server as example
-          -- mason = false, -- set to false if you don't want this server to be installed with mason
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-            },
-          },
-        },
-        bashls = {},
-        clangd = {},
-        cssls = {},
-        html = {},
-        -- jsonls = {}, -- WARN: Covered by LazyVim json extras
-        marksman = {},
-        pyright = {},
-        yamlls = {},
-      },
-      setup = {
-        clangd = function(_, opts)
-          -- https://www.lazyvim.org/configuration/recipes#fix-clangd-offset-encoding
-          opts.capabilities.offsetEncoding = { "utf-16" }
-        end,
-      },
-    },
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       -- treesitter parsers
@@ -80,5 +43,79 @@ return {
         "yaml",
       },
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- Any servers here will be automatically installed with mason and loaded with lspconfig
+        lua_ls = { -- Lua language server as example
+          -- mason = false, -- set to false if you don't want this server to be installed with mason
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+        bashls = {},
+        clangd = {},
+        cssls = {},
+        html = {},
+        -- jsonls = {}, -- WARN: Covered by LazyVim json extras
+        marksman = {},
+        pyright = {
+          settings = {
+            python = {
+              venvPath = os.getenv("HOME") .. "/environments/",
+            },
+          },
+        },
+        yamlls = {},
+      },
+      setup = {
+        clangd = function(_, opts)
+          -- https://www.lazyvim.org/configuration/recipes#fix-clangd-offset-encoding
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        -- Completion
+        nls.builtins.completion.spell,
+        -- Code Action
+        nls.builtins.code_actions.gitsigns,
+        -- Diagnostics
+        nls.builtins.diagnostics.jsonlint,
+        nls.builtins.diagnostics.markdownlint,
+        nls.builtins.diagnostics.ruff,
+        -- nls.builtins.diagnostics.pylint.with({
+        --   extra_args = {
+        --     "--generated-members=numpy.*, torch.*",
+        --     "--disable=missing-class-docstring,missing-function-docstring,line-too-long,logging-fstring-interpolation,wrong-import-order,pointless-string-statement,broad-except",
+        --     "--extension-pkg-whitelist=pydantic",
+        --   },
+        -- }),
+        nls.builtins.diagnostics.write_good, -- "English prose linter"
+        -- Formatters
+        nls.builtins.formatting.autoflake,
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.stylua,
+        -- Hover
+        nls.builtins.hover.dictionary, -- Definitions
+        nls.builtins.hover.printenv, -- Show env values
+      })
+    end,
   },
 }
