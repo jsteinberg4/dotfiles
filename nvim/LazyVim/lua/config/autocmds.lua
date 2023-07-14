c6c165e7da4b19e025cb3961495ff54b5e594b6b
jsteinberg <jesse.steinberg@recordedfuture.com>
2023-07-14 11:07:55 -0400

    nvim: save folds

--- nvim/LazyVim/lua/config/autocmds.lua
+++ nvim/LazyVim/lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Remember code folds on exit
+augroup("RememberFolds", {})
+autocmd("BufWinEnter", {
+  pattern = "*",
+  command = "silent! loadview",
+})
+autocmd("BufWinLeave", {
+  pattern = "*",
+  command = "silent! mkview",
+})
