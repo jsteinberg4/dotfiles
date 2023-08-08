f8a7e71f4c731766c8a50a54d4a307e7f0e88c59
jsteinberg <jesse.steinberg@recordedfuture.com>
2023-08-08 13:50:14 -0400

    update colors

--- nvim/LazyVim/lua/plugins/colors.lua
+++ nvim/LazyVim/lua/plugins/colors.lua
@@ -1,7 +1,8 @@
 -- NOTE: Configure colorscheme stuff
 return {
   { "shaunsingh/nord.nvim", lazy = false },
+  { "sainnhe/gruvbox-material", lazy = false },
   { "catppuccin", lazy = false },
-  { "folke/tokyonight.nvim", lazy = true },
+  { "folke/tokyonight.nvim", lazy = false },
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-frappe" } },
 }
