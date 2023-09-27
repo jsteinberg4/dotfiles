af97acc3699fd0370075664162dfc41842fb0a42
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-09-26 23:29:15 -0400

    center instead of top cursor on search/etc

--- nvim/LazyVim/lua/config/keymaps.lua
+++ nvim/LazyVim/lua/config/keymaps.lua
@@ -21,10 +21,10 @@ end
 map("n", "J", "mzJ`z", { desc = "Join lines w/o moving cursor" })
 map("n", "gg", "ggzz", { desc = "GOTO top, center cursor" })
 map("n", "G", "Gzz", { desc = "GOTO bottom, center cursor" })
-map("n", "<C-d>", "<C-d>zt", { desc = "Page down, top cursor" })
-map("n", "<C-u>", "<C-u>zt", { desc = "Page up, top cursor" })
-map("n", "n", "nztzv", { desc = "Keep cursor centered while searching" })
-map("n", "N", "Nztzv", { desc = "Center cursor while searching" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Page down, center cursor" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Page up, center cursor" })
+map("n", "n", "nzzzv", { desc = "Keep cursor centered while searching" })
+map("n", "N", "Nzzzv", { desc = "Center cursor while searching" })
 
 -- Moving lines
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down", silent = true })
