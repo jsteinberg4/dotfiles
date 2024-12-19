d380b9a27618c43ad31398e953b2fa08dfbc2a5e
Jesse Steinberg <jesse@loris.ai>
2024-12-19 08:39:37 -0800

    plugin updates

--- nvim-reboot/lua/config/lazy.lua
+++ nvim-reboot/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.util.dot" },
     -- Languages
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.docker" },
