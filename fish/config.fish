e5c5833aac0f3ecc58eb1246440672cdb03b50a8
Jesse Steinberg <jesse@loris.ai>
2025-01-02 11:17:05 -0800

    add homebrew and some other basic configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
+# Equivalent to a bashrc file
+# file:///opt/homebrew/Cellar/fish/3.7.1/share/doc/fish/language.html#configuration-files
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # NOTE: Wrap abbreviations/functions if slow. Avoids redefining them, should speed up shell startup
+    # if not set -q _MY_ABBREV_SET
+    #   set -U _MY_ABBREV_SET true
+    # end
+
+    # Example from file:///opt/homebrew/Cellar/fish/3.7.1/share/doc/fish/interactive.html#abbreviations
+    abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+    if type -q lazygit
+        abbr --add lg lazygit
+    end
 end
