d749d75602e2f530f2e6f34fbe22935980b1d74a
Jesse Steinberg <jesse@loris.ai>
2025-03-17 13:28:47 -0400

    delete old configs

--- TODOS.md
+++
@@ -1,2 +0,0 @@
-TODO:
--> Use gnu stow
--- nvim-reboot/lua/config/options.lua
+++
@@ -1,4 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.g.python3_host_prog = vim.fn.stdpath("data") .. "/python-provider/bin/python"
